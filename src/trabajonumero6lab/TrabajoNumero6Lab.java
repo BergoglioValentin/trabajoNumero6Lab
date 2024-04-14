db3cbdee37f5f19585025186beaf2ca04111da41
BergoglioValentin <jesusvalentin1997123@gmail.com>
2024-04-14 13:50:51 -0300

    TrabajoListo

--- src/trabajonumero6lab/TrabajoNumero6Lab.java
+++ src/trabajonumero6lab/TrabajoNumero6Lab.java
@@ -2,6 +2,7 @@ package trabajonumero6lab;
 public class TrabajoNumero6Lab {
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("");
     }
     
 }
